e95869e499eb833fea77d59b1d389698cdfffc13
Vl-Truh <vladislav.truhanovich@gmail.com>
2022-10-01 17:52:51 +0300

    modified files

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';
-  
-  constructor(private messageService: MessageService,
-    private http: HttpClient) {}
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
@@ -25,9 +21,5 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
-  }
-
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
   }
 }
